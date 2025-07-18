c75b0c606205ecaf6f3b3fa1facad6ad93a0fa6e
Airbone2107 <minhnhat2004hcm@gmail.com>
2025-07-18 09:51:46 +0700

    v0.7.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'screens/main/manga_main_screen.dart';
+import 'app.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Manga Reader',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/', // Route mặc định
-      routes: {
-        '/': (context) => MangaMainScreen(), // Trang chủ
-        '/home': (context) => MangaMainScreen(), // Khai báo route cho nút "home"
-      },
-    );
-  }
+  runApp(MangaReaderApp());
 }
